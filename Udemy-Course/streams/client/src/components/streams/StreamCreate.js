ca991d7bc44baa7c54a9c16aca0fd83d79e7d864
adnansadar <adnansadar11@gmail.com>
2021-03-31 23:11:30 +0530

    validation done

--- Udemy-Course/streams/client/src/components/streams/StreamCreate.js
+++ Udemy-Course/streams/client/src/components/streams/StreamCreate.js
@@ -1,7 +1,64 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
+class StreamCreate extends React.Component {
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  }
+  renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? "error" : null}`;
+    return (
+      <div className={className}>
+        <label>{label}</label>
+        <input
+          // onChange={formProps.input.onChange}
+          // value={formProps.input.value}
+          {...input}
+          autoComplete="off"
+        />
+        {this.renderError(meta)}
+      </div>
+    );
+  };
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+  render() {
+    return (
+      // handleSubmit is a method of redux form
+      <form
+        className="ui form error"
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+      >
+        <Field name="title" component={this.renderInput} label="Enter Text" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter Description"
+        />
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
+}
+
+const validate = (formValues) => {
+  const errors = {};
+  // return empty errors object if no errors else return key value pair
+  if (!formValues.title) {
+    errors.title = "You must enter a title";
+  }
+  if (!formValues.description) {
+    errors.description = "You must enter a description";
+  }
+  return errors;
 };
 
-export default StreamCreate;
+export default reduxForm({ form: "streamCreate", validate })(StreamCreate);
+// reduxForm acts like connect over here, where the form is passed as props to StreamCreate
